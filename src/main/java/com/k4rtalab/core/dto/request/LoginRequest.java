363cb99e09435789c2bf7a4f40018fd2e76de419
LaDaniWapa <urpopolakliente@gmail.com>
2026-06-11 20:29:46 +0200

    feat: DTOs

---
+++ src/main/java/com/k4rtalab/core/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.k4rtalab.core.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/k4rtalab/core/dto/request/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.k4rtalab.core.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank
+    private String username;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
